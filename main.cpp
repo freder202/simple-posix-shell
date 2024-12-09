b6284948029a61d64e33a595c8e9910dc25b6999
Frederic Bilodeau <freder202@live.ca>
2024-12-08 20:07:05 -0500

    Passed the test cases

--- main.cpp
+++ main.cpp
@@ -1,95 +1,49 @@
+// Frederic Bilodeau - Dec. 8, 2024
 
-// #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 using namespace std;
 
 // max 1000 caracters from the spec sheet
 #define MAX_LENGTH_COMMAND_STRING 1000
+// max 100 arguments from the spec sheet
 #define MAX_ARGUMENTS 100
 
 int main(void)
 {
-
-    cout << "Basic Posix Shell\n";
-
+    string inputString;
     while (1)
     {
-        string inputString;
+        cerr << "$ ";
         getline(cin, inputString);
-        cout << "raw input : " << "'" << inputString << "'\n";
 
-        // first processing to remove outer spaces
-
-        if (inputString.size() > 1000)
+        // from the spec sheet I think I need to do it before trimming 
+        if (inputString.size() > MAX_LENGTH_COMMAND_STRING)
         {
             cerr << "Warning : input size is over 1000 chars - Input will be dropped\n";
             continue;
         }
 
-        // todo extract method
         // left trim
         int posStart = 0;
-        while (1)
+        while (inputString[posStart] == ' ')
         {
-            if (inputString[posStart] == ' ')
-            {
-                posStart++;
-            }
-            else
-                break;
+            posStart++;
         }
 
         // right trim
-        int posEnd = 1;
-        while (1)
+        int posEnd = 0;
+        while (inputString[inputString.size() - (posEnd + 1)] == ' ')
         {
-            if (inputString[inputString.size() - posEnd] == ' ')
-            {
-                posEnd++;
-            }
-            else
-                break;
+            posEnd++;
         }
-        posEnd--;
 
-        // cout << inputString.size() << endl;
-        // cout << "striped start " << posStart << endl;
-        // cout << "striped end " << posEnd << endl;
-
+        // splicing the input from the trims found
         int nbChar = inputString.size() - (posEnd + posStart);
         inputString = inputString.substr(posStart, nbChar);
-
-        cout << "before word spliting : " << "'" << inputString << "'\n";
-
-        // word splitting
-        string commandLists[100];
-        int pos = 0;
-        int lastPos = 0;
-        int commandsPos = 0;
-        while (pos <= inputString.size())
-        {
-            if (inputString[pos] == ' ' or inputString[pos] == '\0')
-            {
-                cout << pos << endl;
-
-                commandLists[commandsPos] = inputString.substr(lastPos, (pos - lastPos));
-                lastPos = pos;
-                commandsPos++;
-                while (inputString[pos++] == ' ')
-                {
-                    pos++;
-                }
-            }
-                pos++;
-        }
-
-        for (int i = 0; i < 100; i++)
-        {
-            if (!commandLists[i].empty())
-                cout << "-" << commandLists[i] << "-" << endl;
-        }
 
         // check to exit the shell
         if (inputString.compare("exit") == 0)
@@ -97,8 +51,55 @@ int main(void)
             break;
         }
 
-        cout << "'" << inputString << "'\n";
+        // argument counter
+        int pos = 0;
+        int argCount = -1;
+        while (pos <= inputString.size())
+        {
+            if ((inputString[pos] == ' ' || inputString[pos] == '\0') && inputString[pos - 1] != ' ')
+            {
+                argCount++;
+            }
+            pos++;
+        }
+
+        if (argCount > MAX_ARGUMENTS)
+        {
+            cerr << "Warning : argument size is over 100 chars - Input will be dropped\n";
+            continue;
+        }
+
+        // this is one of the commands that need more processing than only the execvp
+        // https://stackoverflow.com/questions/18686114/cd-command-not-working-with-execvp
+        if (inputString.substr(0, 3) == "cd ")
+        {
+            int cdStart = 2;
+            while (inputString[++cdStart] == ' ')
+                ;
+
+            bool trimMore = inputString[cdStart] == '\'';
+            // the trimMore variable is mutliplying the amount to remove on each side
+            chdir(inputString.substr(cdStart + trimMore, inputString.size() - (cdStart + (trimMore * 2))).c_str());
+        }
+
+        // sequence from lecture 6 of CS110 from Standford
+        int newPid = fork();
+        if (newPid == 0)
+        {
+            // everything is changed to a char * because namespace std is used, which makes string default (make the code clearer)
+            char *arguments[] = {(char *)"/bin/sh", (char *)"-c", (char *)inputString.c_str(), NULL};
+
+            execvp(arguments[0], arguments);
+
+            exit(0);
+        }
+        int statusError;
+        waitpid(newPid, &statusError, 0);
+
+        if (WEXITSTATUS(statusError) != 0)
+        {
+            cerr << "error: command exited with code " << WEXITSTATUS(statusError) << endl;
+        }
     }
-    cout << "correctly exited the shell." << endl;
     return 0;
 }
